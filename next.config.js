07aa21100d5c632680db5d0dc70fd34495956fce
samsisle <samko9522@gmail.com>
2021-09-23 16:36:47 -0700

    Update locales location

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   reactStrictMode: true,
-  localePath: path.resolve('./public/locales'),
+  localePath: path.resolve('./locales'),
 };
