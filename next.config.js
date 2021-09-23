304c298200a12441e6d69557637b4fa05c202318
samsisle <samko9522@gmail.com>
2021-09-23 16:52:03 -0700

    Test locales

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ const path = require('path');
 module.exports = {
   i18n,
   reactStrictMode: true,
-  localePath: path.resolve('/var/task/public/locales'),
+  localePath: path.resolve('./locales'),
 };
